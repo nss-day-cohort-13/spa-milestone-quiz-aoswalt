98ad67e35cc4c90c7493b1bfe3c42f53bdcd1932
Adam Oswalt <adam.oswalt@gmail.com>
2016-05-03 09:37:48 -0500

    Basic Bootstrap styling

--- quiz.js
+++ quiz.js
@@ -4,9 +4,10 @@
 (function() {
   var carlotDiv = document.getElementById("carlot");
   function populatePage(inventory) {
-    inventory.forEach(function(car) {
-      var carCard = document.createElement("div");
-      carCard.className = "car-card";
+    for(let carI = 0; carI < inventory.length; ++carI) {
+      let car = inventory[carI]
+      let carCard = document.createElement("div");
+      carCard.className = "car-card col-xs-12 col-sm-6 col-md-4";
       carCard.innerHTML = `
         <p><span class="car-label">Make</span>${car.make}</p>
         <p><span class="car-label">Model</span>${car.model}</p>
@@ -16,7 +17,21 @@
         <p><span class="car-label">Purchased</span>${car.purchased}</p>
         <p><span class="car-label">Description</span><span class="car-desc">${car.description}</span></p>`;
       carlotDiv.appendChild(carCard);
-    });
+
+      //NOTE(adam): clearfix for small 2 column layout
+      if(carI % 2 === 1) {
+        let clearFix = document.createElement("div");
+        clearFix.className = "clearfix visible-sm-block";
+        carlotDiv.appendChild(clearFix);
+      }
+
+      //NOTE(adam): clearfix for medium 3 column layout
+      if(carI % 3 === 2) {
+        let clearFix = document.createElement("div");
+        clearFix.className = "clearfix visible-md-block";
+        carlotDiv.appendChild(clearFix);
+      }
+    }
     CarLot.resetStyles();
     CarLot.activateEvents();
   }
