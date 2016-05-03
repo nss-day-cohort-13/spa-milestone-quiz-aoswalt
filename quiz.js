b4ecaf9459d70b9a2adb391ccb9c5a863b01f68f
Adam Oswalt <adam.oswalt@gmail.com>
2016-05-03 10:03:07 -0500

    Change to Bootstrap rows per requirements

--- quiz.js
+++ quiz.js
@@ -4,10 +4,18 @@
 (function() {
   var carlotDiv = document.getElementById("carlot");
   function populatePage(inventory) {
+    let row;
     for(let carI = 0; carI < inventory.length; ++carI) {
-      let car = inventory[carI]
+      //NOTE(adam): new row for every 3 cards
+      if(carI % 3 === 0) {
+        row = document.createElement("div");
+        row.className = "row";
+        carlotDiv.appendChild(row);
+      }
+
+      let car = inventory[carI];
       let carCard = document.createElement("div");
-      carCard.className = "car-card col-xs-12 col-sm-6 col-md-4";
+      carCard.className = "car-card col-xs-4";
       carCard.innerHTML = `
         <p><span class="car-label">Make</span>${car.make}</p>
         <p><span class="car-label">Model</span>${car.model}</p>
@@ -16,22 +24,8 @@
         <p><span class="car-label">Color</span>${car.color}</p>
         <p><span class="car-label">Purchased</span>${car.purchased}</p>
         <p><span class="car-label">Description</span><span class="car-desc">${car.description}</span></p>`;
-      carlotDiv.appendChild(carCard);
+      row.appendChild(carCard);
       carCard.style.borderColor = car.color;
-
-      //NOTE(adam): clearfix for small 2 column layout
-      if(carI % 2 === 1) {
-        let clearFix = document.createElement("div");
-        clearFix.className = "clearfix visible-sm-block";
-        carlotDiv.appendChild(clearFix);
-      }
-
-      //NOTE(adam): clearfix for medium+ 3 column layout
-      if(carI % 3 === 2) {
-        let clearFix = document.createElement("div");
-        clearFix.className = "clearfix visible-md-block visible-lg-block";
-        carlotDiv.appendChild(clearFix);
-      }
     }
     CarLot.resetStyles();
     CarLot.activateEvents();
